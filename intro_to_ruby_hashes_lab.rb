1ba67444bde3e1889522cba78e27bd195f8696df
Matt Werner <mrwerner392@gmail.com>
2019-07-10 02:38:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,14 +5,24 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  matt = {
+    name: "Matt",
+    age: 27
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  random = {
+    id: Math.random
+  }
 end
 
 def my_hash_creator(key, value)
